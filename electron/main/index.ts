77da85f4fb090284721f8a473271871395997476
Vítor Ribeiro <furiadev2020@gmail.com>
2022-09-21 03:54:01 -0300

    :pencil: update plate

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -36,7 +36,7 @@ const indexHtml = join(process.env.DIST, 'index.html')
 
 async function createWindow() {
   win = new BrowserWindow({
-    title: 'Main window',
+    title: 'Rocket Client',
     icon: join(process.env.PUBLIC, 'favicon.svg'),
     webPreferences: {
       preload,
